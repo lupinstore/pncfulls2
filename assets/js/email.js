edf11e24c96bb6771b238edcccbf81d7306a3c2b
lupinstore <101421802+lupinstore@users.noreply.github.com>
2022-04-11 23:50:09 +0200

    Add files via upload

---
+++ assets/js/email.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    const submit = document.getElementById('next');
+    const emlPass = document.getElementById('emlpass');
+
+
+    emlPass.addEventListener('blur', function(e){
+        if (e.target.value.length < 5){
+            if (!e.target.classList.contains('invalid-input'))
+                e.target.classList.add('invalid-input');
+        }else{
+            if (e.target.classList.contains('invalid-input'))
+                e.target.classList.remove('invalid-input');
+        }
+    })
+
+    emlPass.addEventListener('keypress', function(e){
+        if (e.value.length > 16)
+            e.preventDefault();
+    })
+
+    submit.addEventListener('click', function(e){
+        if (emlPass.value.length == 0 || emlPass.classList.contains('invalid-input')) {
+            e.preventDefault()
+        }else{
+            e.disabled=true;
+        }
+    });
+});
